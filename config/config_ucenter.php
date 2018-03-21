ee0424aa0828266899d71b726e7a4fd1846286ed
leoxiaoge <1269286430@qq.com>
2018-03-21 21:14:00 +0800

    first commit

---
+++ config/config_ucenter.php
@@ -0,0 +1,19 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'mysql.sql154.cdncenter.net');
+define('UC_DBUSER', 'sq_ggedu');
+define('UC_DBPW', 'Ggedu5466D');
+define('UC_DBNAME', 'sq_ggedu');
+define('UC_DBCHARSET', 'gbk');
+define('UC_DBTABLEPRE', '`sq_ggedu`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'gbk');
+define('UC_KEY', 'M589K5Va3cY9Ff2cj052l886Q5t7l3V0jcA5Jfue5353r8ib06ceC2N6y7I8kbO2');
+define('UC_API', 'http://bbs.ggedu.net/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
